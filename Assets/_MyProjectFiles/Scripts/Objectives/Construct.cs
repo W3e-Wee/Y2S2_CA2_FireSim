dc30c1ff9b85db5f85c8ce4d29bbfac494c7b0c3
Cheong Xuan Wei <64126391+XuanWei-CPX@users.noreply.github.com>
2023-02-13 19:13:27 +0800

    URP + Wall Repair + Level 2 #8 & #9
    + Converted Materials to URP
    + Feedback on repairing walls using materials
    + First ver of Level 2
    + Fixed Merge conflicts

--- Assets/_MyProjectFiles/Scripts/Objectives/Construct.cs
+++ Assets/_MyProjectFiles/Scripts/Objectives/Construct.cs
@@ -18,9 +18,13 @@ public class Construct : MonoBehaviour
 	//====================================
 	// [SerializeField] Private Variables
 	//====================================
-	[SerializeField] private Image barFill;
+	//[SerializeField] private Image barFill;
 	[SerializeField] private float totalNeededRepair = 0f;
 	[SerializeField] private float repairAmount = 0f;
+	[Space(20)]
+	[SerializeField] private Material dmgMaterial;
+	[SerializeField] private Renderer dmgWall;
+	[SerializeField] private float alphaColor = 0.3f;
 	//===================
 	// Private Variables
 	//===================
@@ -30,12 +34,16 @@ public class Construct : MonoBehaviour
 	#region Unity Methods
 	protected void Start()
 	{
-		// Get TaskManager gameObject
-		// task = GameObject.FindGameObjectWithTag("TaskManager").GetComponent<TaskManager>();
+        // Get TaskManager gameObject
+        // task = GameObject.FindGameObjectWithTag("TaskManager").GetComponent<TaskManager>();
 
-		// Set bar fill amount
-		// barFill.fillAmount = repairAmount;
-	}
+        // Set bar fill amount
+        // barFill.fillAmount = repairAmount;
+
+        Color color = dmgWall.material.color;
+        color.a = alphaColor;
+        dmgWall.material.color = color;
+    }
 	#endregion
 
 	#region Own Methods
@@ -48,37 +56,61 @@ public class Construct : MonoBehaviour
 
 		// Set progress bar 
 		double fillAmount = Math.Round((repairAmount / totalNeededRepair), 2);
-		// barFill.fillAmount = (float)fillAmount;
+        // barFill.fillAmount = (float)fillAmount;
 
-		// Switch case to show repair progress
-		switch((float)fillAmount)
+        Color color = dmgWall.material.color;
+        color.a = repairAmount;
+        dmgWall.material.color = color;
+
+        if (repairAmount >= totalNeededRepair)
 		{
-			case(.25f):
-				print("Repair at 25%");
 
-				// Visual feedback here
+			print("Wall Repaired");
 
-				break;
-			case(.50f):
-				print("Repair at 50%");
+			// Disable collider
+			// Collider constructCollider = this.GetComponent<BoxCollider>();
+			// constructCollider.enabled = false;
 
-				// Visual feedback here
+			// change the layer
+			int LayerIgnoreRaycast = LayerMask.NameToLayer("Ignore Raycast");
+			gameObject.layer = LayerIgnoreRaycast;
+			// show repaired wall
 
-				break;
-			case(.75f):
-				print("Repair at 75%");
+			// check out objective
+			//task.UpdateConstructTaskList(1);
 
-				// Visual feedback here
+			isRepaired = true;
+			//return true;
+		}
+		// Switch case to show repair progress
+		//switch((float)fillAmount)
+		//{
+		//	case(.25f):
+		//		print("Repair at 25%");
 
-				break;
-			case(1.00f):
-				print("Wall Repaired");
+		//		// Visual feedback here
 
-				// Visual feedback here
-				
-				isRepaired = true; 
-				break;
-		} // End of switch case
+		//		break;
+		//	case(.50f):
+		//		print("Repair at 50%");
+
+		//		// Visual feedback here
+
+		//		break;
+		//	case(.75f):
+		//		print("Repair at 75%");
+
+		//		// Visual feedback here
+
+		//		break;
+		//	case(1.00f):
+		//		print("Wall Repaired");
+
+		//		// Visual feedback here
+
+		//		isRepaired = true; 
+		//		break;
+		//} // End of switch case
 
 		return isRepaired;
 	}
