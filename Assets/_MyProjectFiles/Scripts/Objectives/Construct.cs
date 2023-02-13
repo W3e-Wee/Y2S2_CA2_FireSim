237222b6e8c2777fad5072866fb61f61ce12aab9
Cheong Xuan Wei <64126391+XuanWei-CPX@users.noreply.github.com>
2023-02-14 01:04:38 +0800

    Wall Repair visual feedback #9
    Material feedback to show walls are repairing

--- Assets/_MyProjectFiles/Scripts/Objectives/Construct.cs
+++ Assets/_MyProjectFiles/Scripts/Objectives/Construct.cs
@@ -25,9 +25,20 @@ public class Construct : MonoBehaviour
 	[SerializeField] private Material dmgMaterial;
 	[SerializeField] private Renderer dmgWall;
 	[SerializeField] private float alphaColor = 0.3f;
+	[SerializeField] [Range(0, 1f)] private float intensity;
+	[SerializeField] private float repairSpeed = 0.003f;
+
+	//[SerializeField] private float redColor = 0f;
+	//[SerializeField] private float minus = 1f;
+
+	//[SerializeField] [Range(0f, 1f)] float lerpTime;
+	//[SerializeField] Color myColor;
 	//===================
 	// Private Variables
 	//===================
+	private Color colorEM;
+
+	//private float intensity;
 	// private TaskManager task;
 	#endregion
 	
@@ -43,6 +54,8 @@ public class Construct : MonoBehaviour
         Color color = dmgWall.material.color;
         color.a = alphaColor;
         dmgWall.material.color = color;
+
+		colorEM = dmgWall.material.GetColor("_EmissionColor");
     }
 	#endregion
 
@@ -62,6 +75,15 @@ public class Construct : MonoBehaviour
         color.a = repairAmount;
         dmgWall.material.color = color;
 
+        dmgWall.material.SetColor("_EmissionColor", colorEM * intensity);
+
+		print(colorEM);
+
+		if(intensity > 0)
+		{
+			intensity -= repairSpeed;
+        }
+
         if (repairAmount >= totalNeededRepair)
 		{
 
@@ -74,10 +96,13 @@ public class Construct : MonoBehaviour
 			// change the layer
 			int LayerIgnoreRaycast = LayerMask.NameToLayer("Ignore Raycast");
 			gameObject.layer = LayerIgnoreRaycast;
-			// show repaired wall
+            // show repaired wall
 
-			// check out objective
-			//task.UpdateConstructTaskList(1);
+            // check out objective
+            //task.UpdateConstructTaskList(1);
+
+            intensity = 0f;
+            dmgWall.material.SetColor("_EmissionColor", colorEM * intensity);
 
 			isRepaired = true;
 			//return true;
